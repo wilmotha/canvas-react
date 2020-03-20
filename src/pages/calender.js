d684157997212bd26815f7e93b9f47292e219684
pingnomw <pingnomw@oregonstate.edu>
2020-03-20 12:25:16 -0700

    Calendar now uses Array.map()

--- src/pages/calender.js
+++ src/pages/calender.js
@@ -34,30 +34,14 @@ export default function CalenderPage(props) {
     <div>
 
     <h1>Calendar Events</h1>
-    <h2>{events[0].title}</h2>
-    <h4>Start at: {events[0].start_at}</h4>
-    <h4>End at: {events[0].end_at}</h4>
-    <h4>Location Name: {events[0].location_name}</h4>
-    <h2>{events[1].title}</h2>
-    <h4>Start at: {events[1].start_at}</h4>
-    <h4>End at: {events[1].end_at}</h4>
-    <h4>Location Name: {events[1].location_name}</h4>
-    <h2>{events[2].title}</h2>
-    <h4>Start at: {events[2].start_at}</h4>
-    <h4>End at: {events[2].end_at}</h4>
-    <h4>Location Name: {events[2].location_name}</h4>
-    <h2>{events[3].title}</h2>
-    <h4>Start at: {events[3].start_at}</h4>
-    <h4>End at: {events[3].end_at}</h4>
-    <h4>Location Name: {events[3].location_name}</h4>
-    <h2>{events[4].title}</h2>
-    <h4>Start at: {events[4].start_at}</h4>
-    <h4>End at: {events[4].end_at}</h4>
-    <h4>Location Name: {events[4].location_name}</h4>
-    <h2>{events[5].title}</h2>
-    <h4>Start at: {events[5].start_at}</h4>
-    <h4>End at: {events[5].end_at}</h4>
-    <h4>Location Name: {events[5].location_name}</h4>
+    {events.map((item) => 
+      <div>
+        <h2>{item.title}</h2>
+        <h4>Start at: {item.start_at}</h4>
+        <h4>End at: {item.end_at}</h4>
+        <h4>Location Name: {item.location_name}</h4>
+      </div>
+    )}
 
       {console.log("Events: ", events)}
 
