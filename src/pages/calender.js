395febbf1ec1aed9912781cea9ab8d70825554bf
Austin Wilmoth <wilmotha@oregonstate.edu>
2020-03-26 22:42:30 -0700

    cleaned up calender code

--- src/pages/calender.js
+++ src/pages/calender.js
@@ -32,67 +32,15 @@ export default function CalenderPage(props) {
 
   return (
     <div>
-    <h1>Calendar Events</h1>
-    {events.map(event => (
-      <>
-      <h2>{event.title}</h2>
-      <h4>Start at: {event.start_at}</h4>
-      <h4>End at: {events.end_at}</h4>
-      <h4>Location Name: {event.location_name}</h4>  
-      </>
-    ))}
-    {/* <h2>{events[0].title}</h2>
-    <h4>Start at: {events[0].start_at}</h4>
-    <h4>End at: {events[0].end_at}</h4>
-    <h4>Location Name: {events[0].location_name}</h4>
-    <h2>{events[1].title}</h2>
-    <h4>Start at: {events[1].start_at}</h4>
-    <h4>End at: {events[1].end_at}</h4>
-    <h4>Location Name: {events[1].location_name}</h4>
-    <h2>{events[2].title}</h2>
-    <h4>Start at: {events[2].start_at}</h4>
-    <h4>End at: {events[2].end_at}</h4>
-    <h4>Location Name: {events[2].location_name}</h4>
-    <h2>{events[3].title}</h2>
-    <h4>Start at: {events[3].start_at}</h4>
-    <h4>End at: {events[3].end_at}</h4>
-    <h4>Location Name: {events[3].location_name}</h4>
-    <h2>{events[4].title}</h2>
-    <h4>Start at: {events[4].start_at}</h4>
-    <h4>End at: {events[4].end_at}</h4>
-    <h4>Location Name: {events[4].location_name}</h4>
-    <h2>{events[5].title}</h2>
-    <h4>Start at: {events[5].start_at}</h4>
-    <h4>End at: {events[5].end_at}</h4>
-    <h4>Location Name: {events[5].location_name}</h4> */}
-
-      {console.log("Events: ", events)}
-
+      <h1>Calendar Events</h1>
+      {events.map(event => (
+        <>
+        <h2>{event.title}</h2>
+        <h4>Start at: {event.start_at}</h4>
+        <h4>End at: {events.end_at}</h4>
+        <h4>Location Name: {event.location_name}</h4>  
+        </>
+      ))}
     </div>
   );
 }
-
-// class Reminder extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       reminder: "",
-//
-//     };
-//   }
-//   render() {
-//     return (
-//       <div className="Reminder">
-//         <h1>Reminders</h1>
-//         <h3>{this.state.reminder}</h3>
-//         <a href="javascript:;">
-//           Create a reminder
-//         </a>
-//       </div>
-//     );
-//   }
-//
-// }
-//
-// const rootElement = document.getElementById("root");
-// ReactDOM.render(<Reminder />, rootElement);
